2015c4f9a9024cd9c4c72974d99110277d977fa0
Viacheslav Bukhovskyi <bukhovsky@gmail.com>
2022-01-17 16:38:53 +0200

    reactivity black method add

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,20 @@
 
-  Vue.createApp({
+  const app = Vue.createApp({
     data() {
       return {
-        message: ''
-      }
+        recipetext: '',
+        ingridientstext: 'qq'
+      }},
+      methods: {
+     
+        keyupfunction () {
+            this.ingridientstext = "here will be some ingridients"+ this.recipetext
+        
+        }
+      
     }
-  }).mount('#ingridients_recipe_area')
+  })
+
+const vm = app.mount('#ingridients_recipe_area')
+
 
