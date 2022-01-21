eb25f8a6070381171c22e07817f15d78ca70dd40
Viacheslav Bukhovskyi <bukhovsky@gmail.com>
2022-01-21 10:36:22 +0200

    Ingridients selection regular expression add

--- public/js/script.js
+++ public/js/script.js
@@ -8,7 +8,25 @@
       methods: {
      
         keyupfunction () {
-            this.ingridientstext = "here will be some ingridients: "+ this.recipetext
+
+          const textAreaValue = this.recipetext
+  
+          const arrayOfLines = textAreaValue.split("\n")
+          const arrayOfIngr =[] 
+     
+      
+          for(let i = 0;i < arrayOfLines.length;i++) 
+          {
+              let x = arrayOfLines[i].match(/^.+ +\d+ +гр.*$/)
+              if ( x != null) {
+          arrayOfIngr.push(arrayOfLines[i])
+          console.log(arrayOfLines[i])
+          this.ingridientstext = arrayOfIngr
+                                }
+          }
+
+
+            //this.ingridientstext = "here will be some ingridients: "+ this.recipetext
         
         }
       
