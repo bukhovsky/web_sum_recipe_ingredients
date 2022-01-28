7b3ef077242a4dc162d997b4227a3323f4429ea4
Viacheslav Bukhovskyi <bukhovsky@gmail.com>
2022-01-28 17:29:48 +0200

    Ingridients sum funct partly ready

--- public/js/script.js
+++ public/js/script.js
@@ -21,6 +21,8 @@ const app = Vue.createApp({
           //this.ingridientstext = arrayOfIngr
 
 const ingrConsolidated = []
+const ingrNames = []
+const uniqIngrNames = []
 let y = 0
 while (y < arrayOfIngr.length) {
 
@@ -29,13 +31,34 @@ const weightEnd = arrayOfIngr[y].search(/ +гр/)
 const ingrName = arrayOfIngr[y].slice(0, weightStart - 1)
 const ingrWeight = arrayOfIngr[y].slice(weightStart, weightEnd)
 const ingrSet = [ingrName, ingrWeight]
+
 ingrConsolidated.push(ingrSet)
+ingrNames.push(ingrName)
 y+=1
 }
-this.ingridientstext = ingrConsolidated
+
+// delete ingridients names duplicated
+ingrNames.forEach((c) => {
+     if (!uniqIngrNames.includes(c)) {
+      uniqIngrNames.push(c)
+    }
+                   })
+
+uniqIngrNames.forEach((c) => {
+  let z = 0
+  while (z < ingrSet) {
+    console.log(ingrSet[z][0])
+  }
+
+})
+this.ingridientstext = uniqIngrNames
+// let ingrNames = [...new Set(ingrConsolidated)]
+
+
 
                                }
           }
+        
         
                           }
                 }
